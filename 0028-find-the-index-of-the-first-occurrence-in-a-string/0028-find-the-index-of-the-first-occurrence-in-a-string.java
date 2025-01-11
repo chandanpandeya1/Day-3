a08e86356bb3712f8573fce3cf963f4c8d1f46a6
chandanpandeya1 <70680750+chandanpandeya1@users.noreply.github.com>
2025-01-11 12:50:31 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int strStr(String haystack, String needle) {
+        
+
+        //first find first char matching to haystackor not 
+        for(int i = 0; i<haystack.length()-needle.length();i++){
+            if(haystack.charAt(i)==needle.charAt(0)){
+
+                //find substring
+                if(haystack.substring(i,needle.length()+i).equals(needle)){
+                    return i;
+                    
+                }
+                
+            }
+        }
+        return -1;
+    }
+}
